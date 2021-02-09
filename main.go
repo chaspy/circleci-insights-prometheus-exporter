3b9ba2454412d082fb804861554e8c8722eaa227
kondo takeshi <chaspy@users.noreply.github.com>
2021-02-10 05:33:08 +0900

    Get summary metrics for a project's workflows

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	branch := "develop"
+	reportingWingow := "last-7-days"
+
+	getCircleCIToken, err := getCircleCIToken()
+	if err != nil {
+		log.Fatal("failed to read Datadog Config: %w", err)
+	}
+
+	url := "https://circleci.com/api/v2/insights/gh/quipper/monorepo/workflows?" + "&branch=" + branch + "&reporting-window=" + reportingWingow
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("Circle-Token", getCircleCIToken)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+//	fmt.Println(res)
+	fmt.Println(string(body))
+
+}
+
+func getCircleCIToken() (string,error) {
+	circleciToken := os.Getenv("CIRCLECI_TOKEN")
+	if len(circleciToken) == 0 {
+		return "",fmt.Errorf("missing environment variable CIRCLECI_TOKEN")
+	}
+
+	return circleciToken, nil
+}
