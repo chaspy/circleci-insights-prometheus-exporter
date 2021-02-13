f1538fa57f5ed8fb464a7e84d5614717e4359911
kondo takeshi <chaspy@users.noreply.github.com>
2021-02-14 04:56:30 +0900

    Export workflow jobs insight metrics

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 
 //nolint:gochecknoglobals
 var (
-	successRate = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	wfSuccessRate = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "circleci_custom",
 		Subsystem: "workflow_insight",
 		Name:      "success_rate",
@@ -26,7 +26,7 @@ var (
 	},
 		[]string{"workflow", "repo", "branch"},
 	)
-	durationMetricsMin = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	wfDurationMetricsMin = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "circleci_custom",
 		Subsystem: "workflow_insight",
 		Name:      "duration_metrics_min",
@@ -34,7 +34,7 @@ var (
 	},
 		[]string{"workflow", "repo", "branch"},
 	)
-	durationMetricsMax = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	wfDurationMetricsMax = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "circleci_custom",
 		Subsystem: "workflow_insight",
 		Name:      "duration_metrics_max",
@@ -42,7 +42,7 @@ var (
 	},
 		[]string{"workflow", "repo", "branch"},
 	)
-	durationMetricsMedian = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	wfDurationMetricsMedian = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "circleci_custom",
 		Subsystem: "workflow_insight",
 		Name:      "duration_metrics_median",
@@ -50,7 +50,7 @@ var (
 	},
 		[]string{"workflow", "repo", "branch"},
 	)
-	durationMetricsP95 = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	wfDurationMetricsP95 = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "circleci_custom",
 		Subsystem: "workflow_insight",
 		Name:      "duration_metrics_p95",
@@ -58,7 +58,7 @@ var (
 	},
 		[]string{"workflow", "repo", "branch"},
 	)
-	durationMetricsStandardDeviation = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	wfDurationMetricsStandardDeviation = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "circleci_custom",
 		Subsystem: "workflow_insight",
 		Name:      "duration_metrics_standard_deviation",
@@ -66,7 +66,60 @@ var (
 	},
 		[]string{"workflow", "repo", "branch"},
 	)
+	jobSuccessRate = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "circleci_custom",
+		Subsystem: "job_insight",
+		Name:      "success_rate",
+		Help:      "success rate of workflow",
+	},
+		[]string{"job", "workflow", "repo", "branch"},
+	)
+	jobDurationMetricsMin = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "circleci_custom",
+		Subsystem: "job_insight",
+		Name:      "duration_metrics_min",
+		Help:      "minimum duration metrics",
+	},
+		[]string{"job", "workflow", "repo", "branch"},
+	)
+	jobDurationMetricsMax = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "circleci_custom",
+		Subsystem: "job_insight",
+		Name:      "duration_metrics_max",
+		Help:      "maximum duration metrics",
+	},
+		[]string{"job", "workflow", "repo", "branch"},
+	)
+	jobDurationMetricsMedian = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "circleci_custom",
+		Subsystem: "job_insight",
+		Name:      "duration_metrics_median",
+		Help:      "median of duration metrics",
+	},
+		[]string{"job", "workflow", "repo", "branch"},
+	)
+	jobDurationMetricsP95 = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "circleci_custom",
+		Subsystem: "job_insight",
+		Name:      "duration_metrics_p95",
+		Help:      "95 percentile of duration metrics",
+	},
+		[]string{"job", "workflow", "repo", "branch"},
+	)
+	jobDurationMetricsStandardDeviation = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "circleci_custom",
+		Subsystem: "job_insight",
+		Name:      "duration_metrics_standard_deviation",
+		Help:      "standard deviation of duration metrics",
+	},
+		[]string{"job", "workflow", "repo", "branch"},
+	)
 )
+
+type WorkflowsWithRepo struct {
+	repo     string
+	workflow string
+}
 
 type WorkflowInsightWithRepo struct {
 	repo   string
@@ -101,18 +154,57 @@ type WorkflowInsight struct {
 	} `json:"items"`
 }
 
+type WorkflowJobsInsightWithRepo struct {
+	repo     string
+	branch   string
+	workflow string
+	WorkflowJobsInsight
+}
+
+type WorkflowJobsInsight struct {
+	NextPageToken string `json:"next_page_token"`
+	Items         []struct {
+		Name    string `json:"name"`
+		Metrics struct {
+			TotalRuns        int     `json:"total_runs"`
+			SuccessfulRuns   int     `json:"successful_runs"`
+			TotalCreditsUsed int     `json:"total_credits_used"`
+			FailedRuns       int     `json:"failed_runs"`
+			SuccessRate      float64 `json:"success_rate"`
+			DurationMetrics  struct {
+				Min               int     `json:"min"`
+				Max               int     `json:"max"`
+				Median            int     `json:"median"`
+				Mean              int     `json:"mean"`
+				P95               int     `json:"p95"`
+				StandardDeviation float64 `json:"standard_deviation"`
+			} `json:"duration_metrics"`
+			Throughput float64 `json:"throughput"`
+		} `json:"metrics"`
+		WindowStart time.Time `json:"window_start"`
+		WindowEnd   time.Time `json:"window_end"`
+	} `json:"items"`
+}
+
 func main() {
 	interval, err := getInterval()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	prometheus.MustRegister(successRate)
-	prometheus.MustRegister(durationMetricsMin)
-	prometheus.MustRegister(durationMetricsMax)
-	prometheus.MustRegister(durationMetricsMedian)
-	prometheus.MustRegister(durationMetricsP95)
-	prometheus.MustRegister(durationMetricsStandardDeviation)
+	prometheus.MustRegister(wfSuccessRate)
+	prometheus.MustRegister(wfDurationMetricsMin)
+	prometheus.MustRegister(wfDurationMetricsMax)
+	prometheus.MustRegister(wfDurationMetricsMedian)
+	prometheus.MustRegister(wfDurationMetricsP95)
+	prometheus.MustRegister(wfDurationMetricsStandardDeviation)
+
+	prometheus.MustRegister(jobSuccessRate)
+	prometheus.MustRegister(jobDurationMetricsMin)
+	prometheus.MustRegister(jobDurationMetricsMax)
+	prometheus.MustRegister(jobDurationMetricsMedian)
+	prometheus.MustRegister(jobDurationMetricsP95)
+	prometheus.MustRegister(jobDurationMetricsStandardDeviation)
 
 	http.Handle("/metrics", promhttp.Handler())
 
@@ -131,16 +223,41 @@ func main() {
 }
 
 func snapshot() error {
-	successRate.Reset()
-	durationMetricsMin.Reset()
-	durationMetricsMax.Reset()
-	durationMetricsMedian.Reset()
-	durationMetricsP95.Reset()
-	durationMetricsStandardDeviation.Reset()
+	var workflowWithRepos []WorkflowsWithRepo
+
+	wfSuccessRate.Reset()
+	wfDurationMetricsMin.Reset()
+	wfDurationMetricsMax.Reset()
+	wfDurationMetricsMedian.Reset()
+	wfDurationMetricsP95.Reset()
+	wfDurationMetricsStandardDeviation.Reset()
+
+	jobSuccessRate.Reset()
+	jobDurationMetricsMin.Reset()
+	jobDurationMetricsMax.Reset()
+	jobDurationMetricsMedian.Reset()
+	jobDurationMetricsP95.Reset()
+	jobDurationMetricsStandardDeviation.Reset()
 
 	wfInsightWithRepos, err := getV2WorkflowInsights()
 	if err != nil {
 		return fmt.Errorf("failed to get workflow insights: %w", err)
+	}
+
+	// Extract repository and workflow name
+	for _, wfInsightWithRepo := range wfInsightWithRepos {
+		for _, wfInsight := range wfInsightWithRepo.WorkflowInsight.Items {
+			wfWithRepo := WorkflowsWithRepo{
+				repo:     wfInsightWithRepo.repo,
+				workflow: wfInsight.Name,
+			}
+			workflowWithRepos = append(workflowWithRepos, wfWithRepo)
+		}
+	}
+
+	wfJobsInsightWithRepos, err := getV2WorkflowJobsInsights(workflowWithRepos)
+	if err != nil {
+		return fmt.Errorf("failed to get workflow jobs insights: %w", err)
 	}
 
 	for _, wfInsightWithRepo := range wfInsightWithRepos {
@@ -150,12 +267,29 @@ func snapshot() error {
 				"repo":     wfInsightWithRepo.repo,
 				"branch":   wfInsightWithRepo.branch,
 			}
-			successRate.With(labels).Set(wfInsight.Metrics.SuccessRate)
-			durationMetricsMin.With(labels).Set(float64(wfInsight.Metrics.DurationMetrics.Min))
-			durationMetricsMax.With(labels).Set(float64(wfInsight.Metrics.DurationMetrics.Max))
-			durationMetricsMedian.With(labels).Set(float64(wfInsight.Metrics.DurationMetrics.Median))
-			durationMetricsP95.With(labels).Set(float64(wfInsight.Metrics.DurationMetrics.P95))
-			durationMetricsStandardDeviation.With(labels).Set(wfInsight.Metrics.DurationMetrics.StandardDeviation)
+			wfSuccessRate.With(labels).Set(wfInsight.Metrics.SuccessRate)
+			wfDurationMetricsMin.With(labels).Set(float64(wfInsight.Metrics.DurationMetrics.Min))
+			wfDurationMetricsMax.With(labels).Set(float64(wfInsight.Metrics.DurationMetrics.Max))
+			wfDurationMetricsMedian.With(labels).Set(float64(wfInsight.Metrics.DurationMetrics.Median))
+			wfDurationMetricsP95.With(labels).Set(float64(wfInsight.Metrics.DurationMetrics.P95))
+			wfDurationMetricsStandardDeviation.With(labels).Set(wfInsight.Metrics.DurationMetrics.StandardDeviation)
+		}
+	}
+
+	for _, wfJobsInsightWithRepo := range wfJobsInsightWithRepos {
+		for _, wfJobsInsight := range wfJobsInsightWithRepo.WorkflowJobsInsight.Items {
+			labels := prometheus.Labels{
+				"job":      wfJobsInsight.Name,
+				"workflow": wfJobsInsightWithRepo.workflow,
+				"repo":     wfJobsInsightWithRepo.repo,
+				"branch":   wfJobsInsightWithRepo.branch,
+			}
+			jobSuccessRate.With(labels).Set(wfJobsInsight.Metrics.SuccessRate)
+			jobDurationMetricsMin.With(labels).Set(float64(wfJobsInsight.Metrics.DurationMetrics.Min))
+			jobDurationMetricsMax.With(labels).Set(float64(wfJobsInsight.Metrics.DurationMetrics.Max))
+			jobDurationMetricsMedian.With(labels).Set(float64(wfJobsInsight.Metrics.DurationMetrics.Median))
+			jobDurationMetricsP95.With(labels).Set(float64(wfJobsInsight.Metrics.DurationMetrics.P95))
+			jobDurationMetricsStandardDeviation.With(labels).Set(wfJobsInsight.Metrics.DurationMetrics.StandardDeviation)
 		}
 	}
 
@@ -239,6 +373,80 @@ func getV2WorkflowInsights() ([]WorkflowInsightWithRepo, error) {
 }
 
 func getV2WorkflowInsightsAPI(req *http.Request) ([]byte, int, error) {
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, 0, fmt.Errorf("failed to get response body: %w", err)
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, 0, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	return body, res.StatusCode, nil
+}
+
+func getV2WorkflowJobsInsights(workflowWithRepos []WorkflowsWithRepo) ([]WorkflowJobsInsightWithRepo, error) {
+	var wfJobsInsight WorkflowJobsInsight
+	var wfJobsInsightWithRepos []WorkflowJobsInsightWithRepo
+	var pageToken string
+
+	reportingWindow := getReportingWindow()
+
+	branches, err := getGitHubBranches()
+	if err != nil {
+		return []WorkflowJobsInsightWithRepo{}, fmt.Errorf("failed to read GitHub branch: %w", err)
+	}
+
+	getCircleCIToken, err := getCircleCIToken()
+	if err != nil {
+		log.Fatal("failed to read Datadog Config: %w", err)
+	}
+
+	for _, workflowWithRepo := range workflowWithRepos {
+		for _, branch := range branches {
+			for {
+				url := "https://circleci.com/api/v2/insights/gh/" + workflowWithRepo.repo + "/workflows/" + workflowWithRepo.workflow + "/jobs" + "?branch=" + branch + "&reporting-window=" + reportingWindow + "&page-token" + pageToken
+
+				ctx := context.Background()
+				req, _ := http.NewRequestWithContext(ctx, "GET", url, nil)
+
+				req.Header.Add("Circle-Token", getCircleCIToken)
+
+				body, status, err := getV2WorkflowJobsInsightsAPI(req)
+				if status >= 300 { //nolint:gomnd
+					log.Printf("response status code is not 2xx. status code: %v body: %v. skip.\n", status, string(body))
+					return []WorkflowJobsInsightWithRepo{}, nil
+				}
+
+				if err != nil {
+					return []WorkflowJobsInsightWithRepo{}, fmt.Errorf("failed to call API %w", err)
+				}
+
+				err = json.Unmarshal(body, &wfJobsInsight)
+				if err != nil {
+					return []WorkflowJobsInsightWithRepo{}, fmt.Errorf("failed to parse response body. body %v, err %w", string(body), err)
+				}
+
+				wfJobsInsightWithRepo := WorkflowJobsInsightWithRepo{repo: workflowWithRepo.repo, branch: branch, workflow: workflowWithRepo.workflow, WorkflowJobsInsight: wfJobsInsight}
+				wfJobsInsightWithRepos = append(wfJobsInsightWithRepos, wfJobsInsightWithRepo)
+
+				// pagination
+				if wfJobsInsight.NextPageToken == "" {
+					break
+				} else {
+					pageToken = wfJobsInsight.NextPageToken
+				}
+			}
+		}
+	}
+
+	return wfJobsInsightWithRepos, nil
+}
+
+func getV2WorkflowJobsInsightsAPI(req *http.Request) ([]byte, int, error) {
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, 0, fmt.Errorf("failed to get response body: %w", err)
